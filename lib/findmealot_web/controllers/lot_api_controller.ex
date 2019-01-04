585a500858dbb8a0d3481ae4b7ab2f08f928ab91
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-04 22:23:02 +0200

    implemented controller method

--- lib/findmealot_web/controllers/lot_api_controller.ex
+++ lib/findmealot_web/controllers/lot_api_controller.ex
@@ -1,8 +1,21 @@
 defmodule FindmealotWeb.LotApiController do
   use FindmealotWeb, :controller
 
-  def findLots(conn, %{"given_address" => given_address}) do
+  alias Findmealot.Repo
+  alias Findmealot.Lot
 
+  def findLots(conn, _params) do
+    lots = Repo.all(Lot)
+    |> Enum.map(fn(lot) ->
+      %{
+        street_address: lot.street_address,
+        price_per_hour: lot.price_per_hour,
+        free_lots: lot.free_lots
+      }
+    end)
+
+    conn
+    |> json(%{lots: lots})
   end
 
 end
