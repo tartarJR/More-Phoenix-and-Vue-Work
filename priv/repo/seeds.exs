b1bc2c39c55241213a2d2491007d59bc3004a217
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-05 03:19:30 +0200

    updated seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,7 +16,8 @@ alias Findmealot.{Repo, Lot}
   %{street_address: "Raatuse 22", price_per_hour: 0.2, free_lots: 11},
   %{street_address: "Narva 25", price_per_hour: 0.3, free_lots: 15},
   %{street_address: "Narva 27", price_per_hour: 0.4, free_lots: 9},
-  %{street_address: "Ülikooli 17", price_per_hour: 0.5, free_lots: 20}
+  %{street_address: "Ülikooli 17", price_per_hour: 0.5, free_lots: 20},
+  %{street_address: "Ülikooli 2", price_per_hour: 0.5, free_lots: 0}
  ]
  |> Enum.each(fn lot -> Repo.insert!(Lot.changeset(%Lot{}, %{free_lots: lot.free_lots, price_per_hour: lot.price_per_hour, street_address: lot.street_address})) end)
 
