062e91c60dcdd04b12fb935c3a3d4df635b05cd2
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-05 03:19:45 +0200

    controller refactoring

--- lib/findmealot_web/controllers/lot_api_controller.ex
+++ lib/findmealot_web/controllers/lot_api_controller.ex
@@ -1,16 +1,20 @@
 defmodule FindmealotWeb.LotApiController do
   use FindmealotWeb, :controller
 
+  import Ecto.Query
   alias Findmealot.Repo
   alias Findmealot.Lot
 
   def findLots(conn, _params) do
-    lots = Repo.all(Lot)
+
+    lots = Repo.all(from t in Lot, where: t.free_lots > 0, select: t)
     |> Enum.map(fn(lot) ->
       %{
+        id: lot.id,
         street_address: lot.street_address,
         price_per_hour: lot.price_per_hour,
-        free_lots: lot.free_lots
+        free_lots: lot.free_lots,
+        distance: :null
       }
     end)
 
